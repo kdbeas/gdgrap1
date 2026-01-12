85626f5728eb8e03209c8c43f061d52833a0f35e
kdbeas <kaedentiu@gmail.com>
2026-01-12 19:46:24 +0800

    modified

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -3,14 +3,13 @@
 
 constexpr float PI = 3.14159265358979323846f;
 
-int main(void)
+int main()
 {
     GLFWwindow* window;
 
     if (!glfwInit())
         return -1;
 
-    // Square window with your name
     window = glfwCreateWindow(600, 600, "Kaeden Tiu", NULL, NULL);
     if (!window)
     {
@@ -20,31 +19,31 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
-    // 2D projection
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-1, 1, -1, 1, -1, 1);
     glMatrixMode(GL_MODELVIEW);
 
-    const int sides = 9;
-    const float radius = 0.8f;
-
-    const float angleStep = 2.0f * PI / sides;
-    const float rotationOffset = -PI / 2.0f + angleStep / 2.0f;
+    const float r = 0.8f;
+    const float step = 2.0f * PI / 9.0f;
+    const float offset = -PI / 2.0f + step / 2.0f;
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
         glLoadIdentity();
 
+        // Draw nonagon
         glBegin(GL_LINE_LOOP);
-        for (int i = 0; i < sides; i++)
-        {
-            float angle = i * angleStep + rotationOffset;
-            float x = radius * cosf(angle);
-            float y = radius * sinf(angle);
-            glVertex2f(x, y);
-        }
+            glVertex2f(r * cosf(0 * step + offset), r * sinf(0 * step + offset));
+            glVertex2f(r * cosf(1 * step + offset), r * sinf(1 * step + offset));
+            glVertex2f(r * cosf(2 * step + offset), r * sinf(2 * step + offset));
+            glVertex2f(r * cosf(3 * step + offset), r * sinf(3 * step + offset));
+            glVertex2f(r * cosf(4 * step + offset), r * sinf(4 * step + offset));
+            glVertex2f(r * cosf(5 * step + offset), r * sinf(5 * step + offset));
+            glVertex2f(r * cosf(6 * step + offset), r * sinf(6 * step + offset));
+            glVertex2f(r * cosf(7 * step + offset), r * sinf(7 * step + offset));
+            glVertex2f(r * cosf(8 * step + offset), r * sinf(8 * step + offset));
         glEnd();
 
         glfwSwapBuffers(window);
